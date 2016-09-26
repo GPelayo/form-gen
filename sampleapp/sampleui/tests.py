92eba365673ccdf85eaf2cc382d0a108f79add03
Geryl <gpelayo109@gmail.com>
2016-09-25 18:48:43 -0700

    -Further modularized tests -Removed redundant settings for form -Added Tests for blank font size or target field inputs

--- sampleapp/sampleui/tests.py
+++ sampleapp/sampleui/tests.py
@@ -13,6 +13,13 @@ extracted_dir = os.path.join(test_download_folder, 'gen')
 
 
 class MainPageTest(TestCase):
+    FONT_SIZE_INPUT_ID = 'id_font_size'
+    FONT_TYPE_INPUT_ID = 'id_font_type'
+    CSV_INPUT_ID = 'id_csv_file'
+    TARGET_FIELD_ID = 'id_target_field'
+    IMAGE_INPUT_ID = 'id_template_image'
+    SUBMIT_BUTTON_ID = 'submit_btn'
+
     browser = None
     co = ChromeOptions()
     desired_caps = {'download.default_directory': test_download_folder}
@@ -39,23 +46,60 @@ class MainPageTest(TestCase):
                          msg='Please run formgen server at {}'.format(self.site_address))
 
     def test_basic_input(self):
-        top_left_x = 214
-        top_left_y = 232
-        bottom_right_x = 398
-        bottom_right_y = 275
-
         sample_img = os.path.join(FORMGEN_DIR, 'config', 'assets', 'templates', 'sample.png')
         sample_csv = os.path.join(FORMGEN_DIR, 'data', 'api', 'sample', 'dummy-data.csv')
-        self.browser.find_element_by_id('id_template_image').send_keys(sample_img)
-        self.browser.find_element_by_id('id_font_type').send_keys('Courier')
-        self.browser.find_element_by_id('id_font_size').send_keys('22')
-        self.browser.find_element_by_id('id_csv_file').send_keys(sample_csv)
-        self.browser.find_element_by_id('id_target_field').send_keys('name')
+        self.browser.find_element_by_id(self.IMAGE_INPUT_ID).send_keys(sample_img)
+        self.browser.find_element_by_id(self.FONT_TYPE_INPUT_ID).send_keys('Courier')
+        self.browser.find_element_by_id(self.FONT_SIZE_INPUT_ID).send_keys('22')
+        self.browser.find_element_by_id(self.CSV_INPUT_ID).send_keys(sample_csv)
+        self.browser.find_element_by_id(self.TARGET_FIELD_ID).send_keys('name')
+        self.input_box_selection(214, 232, 398, 275)
+        self.browser.find_element_by_class_name(self.SUBMIT_BUTTON_ID).click()
+        self.assert_no_django_validation_errors()
+        failed_images = self.check_zip_file()
+        self.assertTrue(not failed_images, "The following images failed the test.\n" + "\n".join(failed_images))
 
+    def test_no_font_size_input(self):
+        test_invalid_field = self.FONT_SIZE_INPUT_ID
+        sample_img = os.path.join(FORMGEN_DIR, 'config', 'assets', 'templates', 'sample.png')
+        sample_csv = os.path.join(FORMGEN_DIR, 'data', 'api', 'sample', 'dummy-data.csv')
+        self.browser.find_element_by_id(self.IMAGE_INPUT_ID).send_keys(sample_img)
+        self.browser.find_element_by_id(self.FONT_TYPE_INPUT_ID).send_keys('Courier')
+        self.browser.find_element_by_id(self.CSV_INPUT_ID).send_keys(sample_csv)
+        self.browser.find_element_by_id(self.TARGET_FIELD_ID).send_keys('name')
+        self.input_box_selection(214, 232, 398, 275)
+        self.browser.find_element_by_class_name(self.SUBMIT_BUTTON_ID).click()
+        self.assert_element_has_html5_validation_error([test_invalid_field])
+
+    def test_no_target_field_input(self):
+        test_invalid_field = self.TARGET_FIELD_ID
+        sample_img = os.path.join(FORMGEN_DIR, 'config', 'assets', 'templates', 'sample.png')
+        sample_csv = os.path.join(FORMGEN_DIR, 'data', 'api', 'sample', 'dummy-data.csv')
+        self.browser.find_element_by_id(self.IMAGE_INPUT_ID).send_keys(sample_img)
+        self.browser.find_element_by_id(self.FONT_TYPE_INPUT_ID).send_keys('Courier')
+        self.browser.find_element_by_id(self.FONT_SIZE_INPUT_ID).send_keys('22')
+        self.browser.find_element_by_id(self.CSV_INPUT_ID).send_keys(sample_csv)
+        self.input_box_selection(214, 232, 398, 275)
+        self.browser.find_element_by_class_name(self.SUBMIT_BUTTON_ID).click()
+        self.assert_element_has_html5_validation_error([test_invalid_field])
+
+    def input_box_selection(self, top_left_x, top_left_y, bottom_right_x, bottom_right_y):
         with open(os.path.join(APP_DIR, 'fireboxselect.js'), 'r') as jsfile:
             self.browser.execute_script(jsfile.read(), top_left_x, top_left_y, bottom_right_x, bottom_right_y)
-        self.browser.find_element_by_class_name('submit_btn').click()
 
+    def assert_no_django_validation_errors(self):
+        error_count = len(self.browser.find_elements_by_css_selector("ul.errorlist li"))
+        self.assertEqual(error_count, 0, "There are {} errors.".format(error_count))
+
+    def assert_element_has_html5_validation_error(self, fields: list):
+        invalid_input_list = self.browser.find_elements_by_css_selector("input:invalid")
+        self.assertEqual(len(invalid_input_list), len(fields))
+        for field_input in fields:
+            for invalid_input in invalid_input_list:
+                self.assertEqual(invalid_input.get_attribute('id'), field_input,
+                                 "Element {} doesn't correctly validate input".format(field_input))
+
+    def check_zip_file(self):
         while True:
             if os.path.exists(zip_filepath):
                 break
@@ -79,6 +123,7 @@ class MainPageTest(TestCase):
             if self.file_hashes[file] != hash:
                 failed_images.append(file)
         self.assertTrue(not failed_images, "The following images failed the test.\n" + "\n".join(failed_images))
+        return failed_images
 
     def tearDown(self):
         self.browser.close()
